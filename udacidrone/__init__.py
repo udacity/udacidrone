b8a348b501b9209ab038df1e440d0951731bb211
Dominique Luna <dluna132@gmail.com>
2018-01-16 11:29:15 -0500

    ci fix

--- udacidrone/__init__.py
+++ udacidrone/__init__.py
@@ -1,8 +1,8 @@
-import logging
+# import logging
 
 from .drone import Drone  # noqa: F401
 from .frame_utils import global_to_local, local_to_global  # noqa: F401
-# from .logging import Logger  # noqa: F401
+from .logging import Logger  # noqa: F401
 
 # Default logger to prevent 'No handler found' warning.
 # TODO: make use of this, i.e. replace print statements
