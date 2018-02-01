16173fcb7c8b46525960aeb1bc13fd6c1b2865f7
Dominique Luna <dluna132@gmail.com>
2018-02-01 17:53:43 -0500

    simplify setup.py, add website tutorial links

--- setup.py
+++ setup.py
@@ -1,36 +1,26 @@
 import platform
-import sys
-
 from setuptools import find_packages, setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError, OSError) as e:
-    sys.stderr.write('Failed to convert README.md to rst:\n  {}\n'.format(e))
-    sys.stderr.flush()
-    long_description = open('README.md').read()
-
-__version__ = '0.1.0'
+readme = open('README.md').read()
 
 setup(
     name='udacidrone',
-    version=__version__,
+    version='0.1.0',
     description="Drone API for Udacity's Flying Car Nanodegree",
-    long_description=long_description,
+    long_description=readme,
     packages=find_packages(exclude=('tests*',)),
     url='https://github.com/udacity/udacidrone',
     author='Udacity FCND Team',
     # TODO: Add team email
     author_email='',
     install_requires=[
-        'numpy>=1.10',
-        'future>=0.16.0',
-        'lxml>=4.1.1',
-        'pymavlink>=2.2',
-        'utm>=0.4',
-        'websockets>=4.0.1',
-    ] + (['uvloop>=0.9.1'] if platform.system() is not 'Windows' else []),
+        'numpy>=1.14',
+        'future==0.16.0',
+        'lxml==4.1.1',
+        'pymavlink==2.2',
+        'utm==0.4',
+        'websockets==4.0.1',
+    ] + (['uvloop==0.9.1'] if platform.system() is not 'Windows' else []),
     tests_require=['flake8', 'pytest'],
     keywords='drone api udacity flying car quadrotor',
     license='MIT License',
