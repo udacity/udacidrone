a2211f334a497a03449bf7c6f6d767b88a4bffc8
Dominique Luna <dluna132@gmail.com>
2018-02-07 13:36:25 -0500

    Update setup.py
    pymavlink 2.2 -> 2.2.8. Hopefully this will fix some ubuntu issues

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'numpy>=1.12',
         'future==0.16.0',
         'lxml==4.1.1',
-        'pymavlink==2.2',
+        'pymavlink==2.2.8',
         'utm==0.4',
         'websockets==4.0.1',
     ] + (['uvloop==0.9.1'] if platform.system() is not 'Windows' else []),
